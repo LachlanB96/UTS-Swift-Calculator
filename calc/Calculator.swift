39dd03f34e266a7dc4a7b3789569c91e724fb001
Lachlan <lachlan@lachlanb.com>
2021-03-20 21:11:51 +1100

    precedence added

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 class Calculator {
+    var expression:[String];
     
+    var validChars = ["+", "-", "/", "x", "%"]
     /// For multi-step calculation, it's helpful to persist existing result
-    var currentResult = 0;
+    init(expression:[String]){
+        self.expression = expression;
+        //print(self.expression);
+    }
+    
+    //var currentResult:[String];
     
     /// Perform Addition
     ///
@@ -27,9 +34,128 @@ class Calculator {
         return no1 + no2;
     }
     
-    func calculate(args: [String]) -> String {
+    func sub(no1: Int, no2: Int) -> Int {
+        return no1 - no2;
+    }
+    
+    func multi(no1: Int, no2: Int) -> Int {
+        return no1 * no2;
+    }
+    
+    func divi(no1: Int, no2: Int) -> Int {
+        return no1 / no2;
+    }
+    
+    func mod(no1: Int, no2: Int) -> Int {
+        return no1 % no2;
+    }
+    
+    func operate(currentResult:[String], index:Int, newValue:String) -> [String]{
+        var expression = currentResult;
+        expression[index] = newValue;
+        expression.remove(at: index+1);
+        expression.remove(at: index-1);
+        //print(expression);
+        return expression;
+    }
+    
+    func removePositiveOperand(expression:[String]) -> [String] {
+        var tempExpression = expression;
+        if(tempExpression.count == 1){
+            if(tempExpression[0].hasPrefix("+")){
+                tempExpression[0] = String(tempExpression[0].dropFirst())
+                //return expression[0].dropFirst()
+                return tempExpression;
+            }
+        }
+        return expression;
+    }
+
+    func checkValidInput(expression:[String]){
+        for operand in expression{
+            if(validChars.contains(operand)){
+                print(operand)
+            }
+            print(Int(operand))
+            if Int(operand) != nil{
+                print(operand)
+            }
+        }
+    }
+    
+    func calculate() -> String {
         // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
-        print(args)
-        return(String(add(no1: 1, no2: 2)))
+        
+        var i = 0;
+        var currentResult = expression;
+        var operand:String;
+        checkValidInput(expression: currentResult);
+        currentResult = removePositiveOperand(expression: currentResult);
+        var precedance1 = true;
+        while precedance1 == true{
+            if (i == currentResult.count){
+                precedance1 = false;
+                break;
+            }
+            operand = currentResult[i];
+            //print(operand)
+            if (i % 2 == 1 && i < expression.count-1){
+                switch operand{
+                case "%":
+                    currentResult = operate(currentResult: currentResult, index: i, newValue: String(mod(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!)))
+                    i = 0;
+                case "x":
+                    currentResult[i] = String(multi(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!));
+                    currentResult.remove(at: i+1);
+                    currentResult.remove(at: i-1);
+                    i = 0;
+                case "/":
+                    currentResult[i] = String(divi(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!));
+                    currentResult.remove(at: i+1);
+                    currentResult.remove(at: i-1);
+                    i = 0;
+                default:
+                    i += 0;
+                }
+            }
+            i += 1;
+        }
+        i = 0;
+        while currentResult.count > 1{
+            
+            operand = currentResult[i];
+            //print(operand)
+            if (i % 2 == 1 && i < expression.count-1){
+                switch operand{
+                case "%":
+                    currentResult = operate(currentResult: currentResult, index: i, newValue: String(mod(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!)))
+                    i = 0;
+                case "x":
+                    currentResult[i] = String(multi(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!));
+                    currentResult.remove(at: i+1);
+                    currentResult.remove(at: i-1);
+                    i = 0;
+                case "/":
+                    currentResult[i] = String(divi(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!));
+                    currentResult.remove(at: i+1);
+                    currentResult.remove(at: i-1);
+                    i = 0;
+                case "+":
+                    //print("No1: \(currentResult[i-1]). No2: \(currentResult[i+1]). Op: \(currentResult[i])")
+                    currentResult = operate(currentResult: currentResult, index: i, newValue: String(add(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!)))
+                    i = 0;
+                case "-":
+                    currentResult[i] = String(sub(no1: Int(currentResult[i-1])!, no2: Int(currentResult[i+1])!));
+                    currentResult.remove(at: i+1);
+                    currentResult.remove(at: i-1);
+                    i = 0;
+                default:
+                    exit(0)
+                }
+            }
+            i += 1;
+        }
+        //print(currentResult)
+        return(currentResult[0])
     }
 }
--- calc/main.swift
+++ calc/main.swift
@@ -12,14 +12,18 @@ var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
 // Retrieve User Input
-let input1 = Int(args[0])!; // Sample Code Only! Update Required!
+//let input1 = Int(args[0])!; // Sample Code Only! Update Required!
 //let operator = args[1]; // Sample Code Only! Update Required!
-let input2 = Int(args[2])!; // Sample Code Only! Update Required!
+//let input2 = Int(args[2])!; // Sample Code Only! Update Required!
+
+let testArgs1 = ["5", "+", "3", "+", "2", "+", "8", "+", "5", "x", "20", "+", "2", "-", "5", "+", "1", "/", "1"]
+let testArgs2 = ["+44"]
+let testArgs3 = ["99999999999"]
 
 // Initialize a Calculator object
-let calculator = Calculator();
+let calculator = Calculator(expression: args);
 
 // Calculate the result
-let result = calculator.add(no1: input1, no2: input2);
+let result = calculator.calculate();
 
 print(result)
